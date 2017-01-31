141f5db118444ce953c44c369a87de58a008a36f
Vinicius Serpa <vinicius-serpa@live.com>
2017-01-31 16:43:03 -0200

    fixing bootstrap path

---
+++ poc-spring-boot/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into product (id, name, code) values (1, 'Nescau', '1111');
+insert into product (id, name, code) values (2, 'Leite Desnatado', '2222');
+insert into product (id, name, code) values (3, 'Sucrilhos', '3333');
